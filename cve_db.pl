fa20ccaf72307cacd8e40196abb56799f8c2f673
Peter Wirdemo <peter.wirdemo@gmail.com>
2018-03-30 14:35:36 +0200

    added cmdline args to cve_db.pl

--- cve_db.pl
+++ cve_db.pl
@@ -7,12 +7,25 @@ use lib $FindBin::Bin;
 use Cve;
 
 my($debug) = 0;
+my($update) = undef;
+my($dump) = undef;
 
 GetOptions (
-	"debug=i"  => \$debug
+	"debug=i"  => \$debug,
+	"update" => \$update,
+	"dump" => \$dump,
 ) or die("Error in command line arguments\n");
+
+unless ( $update or $dump ) {
+	die "Usage $0 --dump (dump CVE db) --update (updte CVE db) --debug=1-9 (debug level)\n";
+}
 
 my($cve) = new Cve( debug => $debug );
 
-$cve->update_cve_db();
-$cve->dump_cve_db();
+if ( $update ) {
+	$cve->update_cve_db();
+}
+
+if ( $dump ) {
+	$cve->dump_cve_db();
+}
